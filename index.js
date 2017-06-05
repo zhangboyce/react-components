c8c1c1f40b4d8886de958eb1c38f244c6ad37588
Boyce <Boyce.zhang@ccegroup.cn>
2017-06-05 18:21:36 +0800

    add bower

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ import ProgressBar from './components/progress_bar/ProgressBar.jsx';
 import RowInput from './components/form/RowInput.jsx';
 import FormGroup from './components/form/FormGroup.jsx';
 
+import './public/vendor/bootstrap/dist/css/bootstrap.min.css';
+import './public/vendor/font-awesome/css/font-awesome.min.css';
+import './public/vendor/unslider/dist/css/unslider.css';
+import './public/vendor/unslider/dist/css/unslider-dots.css';
+
+import './public/vendor/jquery/dist/jquery.min.js';
+//import './public/vendor/tether/dist/js/tether.min.js';
+//import './public/vendor/bootstrap/dist/js/bootstrap.min.js';
+import './public/vendor/unslider/dist/js/unslider-min.js';
+
 module.exports = {
     ProgressBar,
     RowInput,
