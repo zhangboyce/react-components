f8e63ea5371fcbb37ee91a27fd8cc5f3d528a474
Zhouzhilu <zhilu741741@163.com>
2017-06-06 10:32:43 +0800

    search component

--- example/demo/SearchDemo.jsx
+++ example/demo/SearchDemo.jsx
@@ -5,5 +5,40 @@ import Layout from '../Layout.jsx';
 
 export default class SearchDemo extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            keyword : ''
+        }
+    }
 
+    handleChange = (keyword) => {
+        this.setState({ keyword: keyword })
+    }
+
+    handleSearch = () => {
+
+    }
+
+
+    render() {
+        return (
+            <Layout title="搜索">
+                <p>
+                    <span>普通搜索:</span>
+                    <div className="bs-example" data-example-id="simple-pre">
+                        <div >
+                           <Search keyword={ this.state.keyword }
+                                   onChange={ this.handleChange }
+                                   onSearch={ this.handleSearch } />
+                        </div>
+                        <pre> { '<div>' +
+                                '<Search keyword={ this.state.keyword } onChange={ this.handleChange } onSearch={ this.handleSearch }/>'+
+                                '</div>' }
+                        </pre>
+                    </div>
+                </p>
+            </Layout>
+        );
+    }
 }
