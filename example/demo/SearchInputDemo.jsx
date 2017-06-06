7e8331f9699f6f9a17fb3efef39abec286bffcec
Zhouzhilu <zhilu741741@163.com>
2017-06-06 17:42:59 +0800

    SearchInput Component

--- example/demo/SearchInputDemo.jsx
+++ example/demo/SearchInputDemo.jsx
@@ -9,12 +9,22 @@ export default class SearchInputDemo extends Component {
         return (
             <Layout title="搜索框">
                 <p>
-                    <span>1. 普通搜索:</span>
+                    <span>1. 普通搜索框:</span>
                     <div className="bs-example" data-example-id="simple-pre">
                         <div style={ { border: '1px dashed rgb(196, 196, 196)' } }>
                             <SearchInput onSearch={ keyword => { alert(keyword) } } />
                         </div>
                         <pre> { '<SearchInput onSearch={ keyword => { alert(keyword) } } />' } </pre>
+                    </div>
+                </p>
+                <p>
+                    <span>2. 可以监听onChange事件:</span>
+                    <div className="bs-example" data-example-id="simple-pre">
+                        <div style={ { border: '1px dashed rgb(196, 196, 196)' } }>
+                            <SearchInput placeholder="可以监听onChange事件" onChange={ keyword => { document.getElementById('msg').innerHTML = keyword; } } />
+                        </div>
+                        <div id="msg" style={{ height: '30px',lineHeight:'30px',padding:'4px 22px' }}></div>
+                        <pre> { '<SearchInput onSearch={ keyword => { document.getElementById("msg").innerHTML = keyword; } } />' } </pre>
                     </div>
                 </p>
             </Layout>
